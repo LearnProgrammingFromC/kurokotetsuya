60988a91d8c7f36d67b2be9da369886fc3826d1f
kurokotetsuya <565192673@qq.com>
2013-08-17 14:28:05 +0800

    practice

---
+++ 实验输出小数几位/16.cpp
@@ -0,0 +1,12 @@
+// 16.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int main(int argc, char* argv[])
+{double a;
+a=10000/3.0;
+printf("%f\n",a);
+return 0;
+}
+
